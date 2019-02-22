f64d7bde34a77346590f528b4e86906bfe0c7a5e
Montgomery <montgomerywatts@yahoo.com>
2019-02-21 22:57:02 -0500

    Created the sign-in template and sign-in GET route. Hooked the route up to the WebServer. The POST route hasn't been completed yet, so actually submitting the form gives you a 404.

---
+++ src/main/java/com/webcheckers/ui/GetSignInRoute.java
@@ -0,0 +1,30 @@
+package com.webcheckers.ui;
+
+import spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+public class GetSignInRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(GetSignInRoute.class.getName());
+    private final TemplateEngine templateEngine;
+
+    public GetSignInRoute(final TemplateEngine templateEngine) {
+        this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+        //
+        LOG.config("GetSignInRoute is initialized.");
+    }
+
+    public Object handle(Request request, Response response) {
+        LOG.finer("GetSignInRoute is invoked.");
+        //
+        Map<String, Object> vm = new HashMap<>();
+        vm.put("title", "Sign in!");
+
+
+        // render the View
+        return templateEngine.render(new ModelAndView(vm , "signin.ftl"));
+    }
+}
---
+++ src/main/resources/spark/template/freemarker/signin.ftl
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></meta>
+    <title>Web Checkers | ${title}</title>
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+
+<body>
+<div class="page">
+
+    <h1>Web Checkers | ${title}</h1>
+
+    <!-- Provide a navigation bar -->
+  <#include "nav-bar.ftl">
+
+    <div class="body">
+
+        <form id="signin" action="/signin" method="post">
+            <input type="text" name="name">
+            <input type="submit" value="Sign in">
+        </form>
+
+    </div>
+
+</div>
+</body>
+
+</html>
