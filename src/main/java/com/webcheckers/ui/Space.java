f6b327420dee71c9a642837323115d503574c6ea
dxb9392 <dxb9392@rit.edu>
2019-03-03 22:39:03 -0500

    Spaces validity now set to false when initialized with a piece on them. Occupied spaces now will not allow you to drop a piece on them, similar to the way white spaces show this.

--- src/main/java/com/webcheckers/ui/Space.java
+++ src/main/java/com/webcheckers/ui/Space.java
@@ -13,6 +13,9 @@ public class Space {
 
     /**
      * Creates a new space with the given cellIndex and modelSpace
+     * Note that valid is being set to false here if the space is initially created with a piece in it.
+     * When it comes tim eto implement turns and piece movement, we will need to create the commands to edit the spaces
+     * validity.
      *
      * @param cellIndex Index of the space in the row
      * @param modelSpace The model space to be simulated
@@ -22,6 +25,7 @@ public class Space {
         this.valid = modelSpace.getColor() == ModelSpace.spaceColor.DARK;
         if(modelSpace.isHasPiece()){
             this.piece = new Piece(modelSpace.getPiece());
+            this.valid = false;
         }
     }
 
