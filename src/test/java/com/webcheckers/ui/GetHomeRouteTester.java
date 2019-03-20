903be5c3d8b1922eab944a7312c598f2ecbff9d9
Dan Murray <dqm8494@rit.edu>
2019-03-20 00:57:17 -0400

    Tests for GetHomeRoute and PlayerLobby

---
+++ src/test/java/com/webcheckers/ui/GetHomeRouteTester.java
@@ -0,0 +1,56 @@
+package com.webcheckers.ui;
+
+import java.util.Map;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import spark.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import com.webcheckers.ui.*;
+import com.webcheckers.application.PlayerLobby;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.mockito.Mockito.*;
+
+public class GetHomeRouteTester {
+    PlayerLobby mockPlayerLobby;
+    TemplateEngine mockTemplateEngine;
+    GetHomeRoute CuT;
+    Request mockRequest;
+    Response mockResponse;
+    Session mockSession;
+
+    //@BeforeEach
+    public void setup(){
+        mockPlayerLobby = mock(PlayerLobby.class);
+        mockTemplateEngine = mock(TemplateEngine.class);
+        Request mockRequest = mock(Request.class);
+        Response mockResponse = mock(Response.class);
+        Session mockSession = mock(Session.class);
+        when(mockRequest.session()).thenReturn(mockSession);
+
+        CuT = new GetHomeRoute(mockPlayerLobby, mockTemplateEngine);
+    }
+
+    @Test
+    public void testRequest(){
+        mockPlayerLobby = mock(PlayerLobby.class);
+        mockTemplateEngine = mock(TemplateEngine.class);
+        Request mockRequest = mock(Request.class);
+        Response mockResponse = mock(Response.class);
+        Session mockSession = mock(Session.class);
+        when(mockRequest.session()).thenReturn(mockSession);
+
+        CuT = new GetHomeRoute(mockPlayerLobby, mockTemplateEngine);
+
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(mockTemplateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        CuT.handle(mockRequest, mockResponse);
+
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+        testHelper.assertViewName("home.ftl");
+    }
+}
